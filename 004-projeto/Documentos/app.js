2517222dd4222d3347184ca1cebc96629f5a0810
Rodrigo Franklin Júlio <56088459+rodrigofjulio@users.noreply.github.com>
2022-08-15 20:08:46 -0300

    Add files via upload

--- 004-projeto/Documentos/app.js
+++ 004-projeto/Documentos/app.js
@@ -18,4 +18,10 @@ console.log(`O valor atual de num3 é ${num3}`);
 num4 = num1%num2;
 console.log(num4);
 num3_round = Math.round(num3);
+
+//apresente novamente o valor de num3
 console.log(num3_round);
+
+var num5 = 12312214.12498789;
+num5 = num5.toFixed(2);
+console.log(num5);
