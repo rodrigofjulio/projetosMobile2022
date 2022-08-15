59145340afeb2a2e0d0a0f71149e2fa24418230e
Rodrigo Franklin Júlio <56088459+rodrigofjulio@users.noreply.github.com>
2022-08-15 19:41:11 -0300

    Add files via upload

--- 004-projeto/Documentos/app.js
+++ 004-projeto/Documentos/app.js
@@ -1,9 +1,11 @@
 var num1 = 5;
 var num2 = 2;
-var num3 
+var num3 //futuro numero float
 
 //apresente os valores de num1, num2 e num3.
-console.log(num1 , num2, num3);
+console.log(num1);
+console.log(num2);
+console.log(num3);
 
 
 
@@ -12,3 +14,8 @@ var num3 = num1/num2;
 
 //apresente o valor de num3
 console.log(`O valor atual de num3 é ${num3}`);
+
+num4 = num1%num2;
+console.log(num4);
+num3_round = Math.round(num3);
+console.log(num3_round);
