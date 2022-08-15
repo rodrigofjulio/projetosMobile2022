390c10c9cbd791b54515cfcac3c33a4df065275c
Rodrigo Franklin Júlio <56088459+rodrigofjulio@users.noreply.github.com>
2022-08-15 19:03:00 -0300

    Add files via upload

--- 004-projeto/Documentos/app.js
+++ 004-projeto/Documentos/app.js
@@ -1,3 +1,14 @@
 var num1 = 5;
 var num2 = 2;
-var num3 =  ;
+var num3 
+
+//apresente os valores de num1, num2 e num3.
+console.log(num1 , num2, num3);
+
+
+
+//faça o calculo abaixo:
+var num3 = num1/num2;
+
+//apresente o valor de num3
+console.log(`O valor atual de num3 é ${num3}`);
